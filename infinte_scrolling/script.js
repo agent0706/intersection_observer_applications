b3914cbd90e5b6db39575829376b15b9a8c23cc1
agent0706 <silentassasinagent07@gmail.com>
2022-02-20 00:26:07 +0530

    adding timeout and loading tag

--- infinte_scrolling/script.js
+++ infinte_scrolling/script.js
@@ -14,25 +14,46 @@ const addNewCardToList = (cardsContainer) => {
     return newCard;
 };
 
-const loadMoreCards = () => {
+const addLoadingTag = () => {
+    const loadingElement = document.createElement('div');
+    loadingElement.classList.add('loading_text');
+    loadingElement.textContent = '....Loading more cards';
     const cardsContainer = document.querySelector('.cards-container');
-    const currentLastCard = document.querySelector('.last');
-    for (let i = 0; i < 5; i++) {
-        const newCard = addNewCardToList(cardsContainer);
-        // if the newCard is the last card, observe it and unobserve the previous last card
-        if  (i === 4) {
-            newCard.classList.add('last');
-            observer.unobserve(currentLastCard);
-            observer.observe(newCard);
-        }
-    }
-    currentLastCard.classList.remove('last');
+    cardsContainer.appendChild(loadingElement);
+};
+
+const removeLoadingTag = () => {
+    const loadingElement = document.querySelector('.loading_text');
+    const cardsContainer = document.querySelector('.cards-container');
+    cardsContainer.removeChild(loadingElement);
+};
+
+const loadMoreCards = () => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            const cardsContainer = document.querySelector('.cards-container');
+            const currentLastCard = document.querySelector('.last');
+            for (let i = 0; i < 5; i++) {
+                const newCard = addNewCardToList(cardsContainer);
+                // if the newCard is the last card, observe it and unobserve the previous last card
+                if  (i === 4) {
+                    newCard.classList.add('last');
+                    observer.unobserve(currentLastCard);
+                    observer.observe(newCard);
+                }
+            }
+            currentLastCard.classList.remove('last');
+            resolve();
+        }, 2000);
+    });
 };
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
-            loadMoreCards();
+            addLoadingTag();
+            loadMoreCards()
+            .finally(() => removeLoadingTag());
         }
     });
 }, options);
